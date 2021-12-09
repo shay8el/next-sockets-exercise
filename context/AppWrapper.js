57799b3196da850c042f5b4991be1576b099076a
Shayel <shayel@mixtiles.com>
2021-12-09 17:24:19 +0200

    fix multiple

--- context/AppWrapper.js
+++ context/AppWrapper.js
@@ -2,42 +2,54 @@ import React, { createContext, useContext, useState, useEffect } from 'react'
 import io from 'socket.io-client'
 
 const AppContext = createContext()
-const URL = '/api/searchServer'
+const URL = "/api/searchServer"
 export function AppWrapper({ children }) {
   const [searchSocket, setSearchSocket] = useState(null)
   const [searchResults, setSearchResults] = useState([])
   const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
-    fetch(URL)
-    let socketIo = io()
-    socketIo.on('results-batch', data => {
-      setSearchResults(data)
+    fetch(URL).finally(() => {
+      const socketIo = io('', { autoConnect: false })
+      
+      socketIo.on('connect', () => {
+        const queryId = socketIo.id
+        console.log(`query ${queryId} started`)
+      })
+      socketIo.on('results-batch', data => {
+        const queryId = socketIo.id
+        console.log(`batch for query ${queryId} arrived`)
+        setSearchResults(data)
+      })
+      socketIo.on('found', () => {
+        const queryId = socketIo.id
+        console.log(`query ${queryId} finished`)
+        setIsLoading(false)
+      })
+      socketIo.on('disconnect', () => {
+        console.log('query canceled')
+      })
+      setSearchSocket(socketIo)
+      function cleanup() {
+        socketIo.disconnect()
+      }
+      return cleanup
     })
-    socketIo.on('found', () => {
-      setIsLoading(false)
-    })
-    socketIo.on('disconnect', () => {
-      setIsLoading(false)
-    })
-    setSearchSocket(socketIo)
-    function cleanup() {
-      socketIo.disconnect()
-    }
-    return cleanup
   }, [])
 
   const sendSearchQuery = (data) => {
-    clearSearchResults()
     if (searchSocket){
-      if (isLoading){
+      if (isLoading){ // does still there is a query running?
         console.log('cancel-previous-search')
         searchSocket.emit('stop-search')
       }
+      searchSocket.disconnect()
+      clearSearchResults()
       setIsLoading(true)
+      searchSocket.connect()
       searchSocket.emit('search', data)
     } else {
-      console.log('no searchSocket')
+      console.log('no search socket')
     }
   }
 
--- pages/api/searchServer.js
+++ pages/api/searchServer.js
@@ -5,18 +5,20 @@ const ioHandler = (req, res) => {
   if (!res.socket.server.io) {
     const io = new Server(res.socket.server)
     io.on('connection', socket => {
+      console.log(`query ${socket.id} started`)
       const onResultFetch = (results) => socket.emit('results-batch', results)
       socket.on('search', async(data) => {
         const results = await search(data, onResultFetch)
         socket.emit('found', results)
       })
       socket.on('stop-search', async() => {
-        console.log('cancel-existing-search')
+        socket.disconnect()
+        console.log(`query ${socket.id} canceled`)
       })
     })
     res.socket.server.io = io
   } else {
-    console.log('socket.io already running')
+    console.log('query already running')
   }
   res.end()
 }
